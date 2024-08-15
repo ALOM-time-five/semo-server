20c1a80bb4a697447e15c04e65356ac10f2adc62
Kimseungin0529 <whffkaos007@naver.com>
2024-08-15 17:38:11 +0900

    feat : RedisSubscriber 임시 메세지 처리 로직 구현
    사용 클래스 및 dto 미구현 상태

---
+++ src/main/java/arom_semo/server/domain/chat/service/RedisSubscriber.java
@@ -0,0 +1,36 @@
+package arom_semo.server.domain.chat.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Service;
+
+@Service @Slf4j
+@RequiredArgsConstructor
+public class RedisSubscriber implements MessageListener {
+    private final ObjectMapper objectMapper;
+    private final RedisTemplate redisTemplate;
+    private final SimpMessageSendingOperations messagingTemplate;
+
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        try {
+            String publishMessage = (String) redisTemplate.getStringSerializer().deserialize(message.getBody());
+
+            ChatMessageRequest roomMessage = objectMapper.readValue(publishMessage, ChatMessageRequest.class);
+
+            if (roomMessage.getType().equals(MessageType.TALK)) {
+                GetChatMessageResponse chatMessageResponse = new GetChatMessageResponse(roomMessage);
+                messagingTemplate.convertAndSend("/sub/chat/room/" + roomMessage.getRoomId(), chatMessageResponse);
+            }
+
+        } catch (Exception e) {
+            //throw new ChatMessageNotFoundException();
+            throw new RuntimeException("chat 예외 발생");
+        }
+    }
+}
