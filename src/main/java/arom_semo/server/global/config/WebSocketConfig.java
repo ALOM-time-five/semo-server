48ce7e16654ddcc02259a2f346e2e66b6794686e
Kimseungin0529 <whffkaos007@naver.com>
2024-08-21 15:58:02 +0900

    style : WebSockeyConfigration ->  WebSocketConfig 로 명칭 변경

--- src/main/java/arom_semo/server/global/config/WebSocketConfig.java
+++ src/main/java/arom_semo/server/global/config/WebSocketConfig.java
@@ -1,15 +1,13 @@
 package arom_semo.server.global.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
 @EnableWebSocket
 @EnableWebSocketMessageBroker
-public class WebSocketConfigration  implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(final MessageBrokerRegistry registry) {
