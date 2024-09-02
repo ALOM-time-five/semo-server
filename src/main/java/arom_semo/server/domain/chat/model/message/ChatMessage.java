0e4c8fea2be467a532eda35db0a60c926d166c49
Kimseungin0529 <whffkaos007@naver.com>
2024-09-02 14:51:28 +0900

    feat : ChatRoom 엔티티 설계

--- src/main/java/arom_semo/server/domain/chat/model/message/ChatMessage.java
+++ src/main/java/arom_semo/server/domain/chat/model/message/ChatMessage.java
@@ -1,45 +1,43 @@
 package arom_semo.server.domain.chat.model.message;
 
+import arom_semo.server.domain.chat.model.room.ChatRoom;
 import arom_semo.server.global.model.MongoBaseEntity;
 import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 
 @Getter
 @Document(collection = "chat_messages")
 @NoArgsConstructor
-public class ChatMessage extends MongoBaseEntity /*implements Message*/{
+public class ChatMessage extends MongoBaseEntity{
     @Id
     private String id;
     private String sender;
     private Long userId;
     private String senderImageUrl;
     private String content;
-    private String roomId;
+    @DBRef
+    private ChatRoom chatRoom;
     private MessageType type;
 
     @Builder
-    public ChatMessage(String sender, Long userId, String senderImageUrl, String content, String roomId, MessageType type) {
+    public ChatMessage(String sender, Long userId, String senderImageUrl, String content, ChatRoom chatRoom, MessageType type) {
         this.sender = sender;
         this.userId = userId;
         this.senderImageUrl = senderImageUrl;
         this.content = content;
-        this.roomId = roomId;
+        this.chatRoom = chatRoom;
         this.type = type;
     }
-
-    /*@Override
-    public MessageType getType() {
-        return MessageType.CHAT;
-    }*/
 
     //@Override
     public void process(SimpMessageSendingOperations messagingTemplate) {
 
-        String destination = "/sub/chat/room/" + roomId;
+        String destination = "/sub/chat/room/" + chatRoom.getId();
         if (type == MessageType.CHAT) {
             messagingTemplate.convertAndSend(destination, this);
         } else if (type == MessageType.JOIN) {
--- src/main/java/arom_semo/server/domain/chat/model/room/ChatRoom.java
+++ src/main/java/arom_semo/server/domain/chat/model/room/ChatRoom.java
@@ -1,7 +1,8 @@
 package arom_semo.server.domain.chat.model.room;
 
-import arom_semo.server.global.model.BaseEntity;
+import arom_semo.server.global.model.MongoBaseEntity;
 import jakarta.persistence.Id;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -12,14 +13,22 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 @Document(collection = "chat_rooms")
-public class ChatRoom extends BaseEntity {
+public class ChatRoom extends MongoBaseEntity {
     @Id
     private String id;  // MongoDB에서는 ID로 ObjectId 사용
     private String title;  // 채팅방 이름
-    private List<Long> userIds = new ArrayList<>();  // 채팅방에 속한 사용자 ID 리스트
+    private List<Long> memberIds = new ArrayList<>();  // 채팅방에 속한 사용자 ID 리스트
 
-    public ChatRoom(String title, List<Long> userIds) {
+
+    @Builder
+    public ChatRoom(String title, List<Long> memberIds) {
         this.title = title;
-        this.userIds = userIds;
+        this.memberIds = memberIds;
     }
+
+    public int getMemberCount(){
+        return this.memberIds.size();
+    }
+
+
 }
